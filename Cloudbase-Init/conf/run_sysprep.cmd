39ebb4f4edfe4016f74382c9f9dea3091b7f46a5
miketrojahn <michael.trojahn@educateit.ch>
2023-01-23 09:58:33 +0100

    adds cloudbase-init config

---
+++ Cloudbase-Init/conf/run_sysprep.cmd
@@ -0,0 +1,4 @@
+cd "C:\Program Files \Cloudbase Solutions\Cloudbase-Init\conf"
+ipconfig /release
+c:\windows\system32\sysprep\sysprep.exe /generalize /oobe /mode:vm /shutdown /unattend:unattend.xml
+
